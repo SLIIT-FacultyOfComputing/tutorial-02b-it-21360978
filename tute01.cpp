816d65a11b750ac3e6ea76b0706b90057c625bbf
Gihan_Serasinghe <99310585+it-21360978@users.noreply.github.com>
2022-02-21 18:45:52 +0000

    conver the c++

--- tute01.cpp
+++ tute01.cpp
@@ -17,14 +17,28 @@ printf("Hello World") --> std::cout << "Hello World";
 2.54cm = 1 inch
 */
 
-
+/*
 #include <stdio.h>
 int main() 
 {
     float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
+    printf("Enter a length in cm : "); 
+    scanf("%f",&cm); 
     inches = cm / 2.54;
     printf("Length in inches is %f \n", inches);
     return 0;
-}  
+}  */
+
+#include <iostream>
+int main()
+{
+  float cm;
+  float inches;
+
+  std::cout << "Enter a length in cm :" << '\n';
+  std::cin >> cm;
+
+  inches = cm / 2.54;
+  std::cout << "Length in iches is : " << inches ;
+  return 0;
+}
--- tute02.cpp
+++ tute02.cpp
@@ -11,7 +11,7 @@ OtRate = 1500
 Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
-
+/*
 #include <stdio.h>
 int main()
 {
@@ -41,4 +41,41 @@ int main()
    printf("Net Salary is %f ", netSalary);
   
    return 0;
+}*/
+
+#include <iostream>
+int main()
+{
+   double salary, netSalary;
+   int etype, otHrs, otRate;
+
+   std::cout << "Enter Employee Type :" << '\n';
+   std::cin >> etype ;
+
+   std::cout << "Enter Salary  : " << '\n';
+   std::cin >> salary ;
+
+   std::cout <<"Enter OtHrs : "<< '\n';
+   std::cin >> otHrs ;
+
+   switch (etype) {
+      case 1 :
+          otRate = 1000;
+          break;
+      case 2 :
+          otRate = 1500;
+          break;
+      default :
+          otRate = 1700;
+          break;
+   }
+
+   netSalary = salary + otHrs* otRate;
+   std::cout << "Net Salary is : " << netSalary ;
+
+   return 0;
 }
+
+
+
+
